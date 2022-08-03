9e08a76a9be11208c4940568614cf26d93056400
v-zhangliqun <v-zhangliqun@360.cn>
2022-08-03 11:32:14 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TextView
+
+A description of this package.
---
+++ Tests/TextViewTests/TextViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TextView
+
+final class TextViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TextView().text, "Hello, World!")
+    }
+}
